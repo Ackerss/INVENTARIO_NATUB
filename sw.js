c3c6aa4bc4d883a5e131007dbc03b328944be00a
Ackerss <129917491+Ackerss@users.noreply.github.com>
2025-04-27 20:43:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,16 @@
-const CACHE_NAME = 'inventario-granel-cache-v1';
+const CACHE_NAME = 'inventario-granel-cache-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './potes.json',
+  './','./index.html','./app.js','./manifest.json','./potes.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME)
-          .then(cache => cache.addAll(urlsToCache))
-          .then(() => self.skipWaiting())
-  );
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(resp => resp || fetch(evt.request))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
