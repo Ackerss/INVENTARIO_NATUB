70d53a5d1cdfad4647bce7ea691434b3452f669f
Ackerss <129917491+Ackerss@users.noreply.github.com>
2025-04-27 23:44:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-s
+const CACHE_NAME = 'inventario-granel-cache-v5';   // << aumente sempre que trocar arquivos
+const urlsToCache = [
+  './','./index.html','./app.js','./manifest.json','./potes.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
